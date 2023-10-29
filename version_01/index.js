47c5fb9c65fbe6ae6d478fcf9b9ddd855ae7cefd
salinpaul valooran <salinpv@hotmail.com>
2023-10-30 02:14:47 +0530

    linking progress bar with song but not working

--- version_01/index.js
+++ version_01/index.js
@@ -1,3 +1,8 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function() {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+}
