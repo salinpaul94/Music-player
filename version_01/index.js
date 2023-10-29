e572e0acbc7c4df645c1b906a3d17952bf646862
salinpaul valooran <salinpv@hotmail.com>
2023-10-30 02:19:04 +0530

    contorlling play pause icon

--- version_01/index.js
+++ version_01/index.js
@@ -6,3 +6,15 @@ song.onloadedmetadata = function() {
   progress.max = song.duration;
   progress.value = song.currentTime;
 }
+
+function playpause() {
+  if(ctrlIcon.classList.contains("fa-pause")){
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+  }
+}
